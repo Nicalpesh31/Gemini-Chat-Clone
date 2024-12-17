128fcea15a2a289defa07bdf5030a7a6403386f0
Nicalpesh31 <mca2021.alpeshnichat@asmedu.org>
2024-12-17 12:58:48 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,12 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> c302e5d995d43e468a09e386ed8ee4d143f8ea12
